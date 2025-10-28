0adcb047dd509aaf481c1c1929516231f2701a01
Zheer Barzan <zheerbarzan85@outlook.com>
2025-10-28 22:37:44 +0300

    im nearly out of charge but i made most of the extensions

--- Habitology/Extentions/Date+Exetensions.swift
+++ Habitology/Extentions/Date+Exetensions.swift
@@ -8,5 +8,30 @@
 import SwiftUI
 
 extension Date{
+    var weekday: String {
+        let calendar = Calendar.current
+        let weekday = calendar.weekdaySymbols[calendar.component(.weekday, from: self) - 1]
+        return weekday
+        
+    }
     
+    var StartOfDay: Date {
+        let calendar = Calendar.current
+        return calendar.startOfDay(for: self)
+    }
+    
+    var isToday: Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInToday(self)
+    }
+    
+    static var startDayOfThisMonth: Date {
+        let calender = Calendar.current
+        guard let date = calender.date(from: calender.dateComponents([.year, .month], from: .now)) else{
+            fatalError("cannot retireve the date of this month")
+        }
+        return date
+    }
+    
+    // Todo dates of this month
 }
